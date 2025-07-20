33a2d6310602dae94be56761ff52ae6d5eb07483
ashgw <ashrefgw@proton.me>
2025-07-20 22:51:31 +0100

    remove `precmd`

--- funcs.zsh
+++ funcs.zsh
@@ -2,19 +2,6 @@
 # ================================================================
 #    ZSH FUNCTION COLLECTION
 # ================================================================
-precmd() {
-    # Print the previously configured title
-    print -Pnr -- "$TERM_TITLE"
-
-    # Print a new line before the prompt, but only if it is not the first line
-    if [ "$NEWLINE_BEFORE_PROMPT" = yes ]; then
-        if [ -z "$_NEW_LINE_BEFORE_PROMPT" ]; then
-            _NEW_LINE_BEFORE_PROMPT=1
-        else
-            print ""
-        fi
-    fi
-}
 # Needs SSH
 # generate new SSH keys for github, run this u'll get the pub key copied to ur clipboard,just paste it
 ghkey() {
