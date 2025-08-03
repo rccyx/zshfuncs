3251f56488281ec72ad439e5e86bcb26f473b14e
ashgw <ashrefgw@proton.me>
2025-08-03 22:41:42 +0100

    add git zsh

---
+++ git.zsh
@@ -0,0 +1,27 @@
+# open repo on GitHub
+gitbrowse() {
+  local remote
+  remote=$(git config --get remote.origin.url)
+  remote=${remote/git@github.com:/https:\/\/github.com\/}
+  remote=${remote/.git/}
+  xdg-open "$remote"
+}
+
+
+# git checkout on roids
+gck() {
+  local branch
+  branch=$(git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)' \
+    | fzf --prompt="🌿 checkout branch ⇢ " --preview="git log -n 10 --color=always {}" --height=50%)
+  [[ -n "$branch" ]] && git checkout "$branch"
+}
+compdef _git gck
+
+# git grepper for commits
+ggrep() {
+  local q
+  echo -n "🔍 search term: "; read -r q
+  git log --all --pretty=format:'%C(auto)%h %s %Cgreen(%cr)' -S"$q" |
+    fzf --reverse --preview="echo {} | cut -d' ' -f1 | xargs git show --color=always"
+}
+compdef _git ggrep
