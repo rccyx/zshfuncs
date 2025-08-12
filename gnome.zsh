bf4b8e7287cfc44cbbf83bcaa14d2881470b5f90
ashgw <ashrefgw@proton.me>
2025-08-12 08:34:47 +0100

    add misc commands

---
+++ gnome.zsh
@@ -0,0 +1,8 @@
+apply_gnome(){
+gsettings set org.gnome.desktop.interface gtk-theme "Tokyonight-Dark-Storm-B-MB"
+gsettings set org.gnome.shell.extensions.user-theme name "Tokyonight-Dark-Storm-B-MB"
+gsettings set org.gnome.desktop.interface icon-theme "dracula-icons-main"
+gsettings set org.gnome.desktop.interface font-name "San Francisco 14"
+gsettings set org.gnome.desktop.interface monospace-font-name "San Francisco Mono 13"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "San Francisco Bold 14"
+}
---
+++ misc.zsh
@@ -0,0 +1,13 @@
+precmd() {
+    # Print the previously configured title
+    print -Pnr -- "$TERM_TITLE"
+
+    # Print a new line before the prompt, but only if it is not the first line
+    if [ "$NEWLINE_BEFORE_PROMPT" = yes ]; then
+        if [ -z "$_NEW_LINE_BEFORE_PROMPT" ]; then
+            _NEW_LINE_BEFORE_PROMPT=1
+        else
+            print ""
+        fi
+    fi
+}
