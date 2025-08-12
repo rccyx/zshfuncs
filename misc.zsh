055a15ac313d6259d619dfdf60792355459e0078
ashgw <ashrefgw@proton.me>
2025-08-12 08:46:49 +0100

    add `loco`

--- misc.zsh
+++ misc.zsh
@@ -1,3 +1,12 @@
+# counts the lines of code in your current codebase
+# needs pnpm
+loc() {
+  local dir="${1:-.}"
+  pnpm dlx cloc "$dir" \
+    --exclude-dir='node_modules,dist,out,.next,build,.turbo,.vercel,.git,.venv,__pycache__,target,vendor' \
+    --not-match-f='.*lock|.*min.js|.*.svg|.*.map|.*.log'
+}
+
 precmd() {
     # Print the previously configured title
     print -Pnr -- "$TERM_TITLE"
