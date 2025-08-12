3cbb30bc217780841d58bdaf6532b1a9a233f549
ashgw <ashrefgw@proton.me>
2025-08-12 09:03:15 +0100

    final cleanup

--- misc.zsh
+++ misc.zsh
@@ -1,3 +1,30 @@
+# shows pretty `man` page.
+man () {
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+      man "$@"
+}
+
+
+ remindme() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: remindme <seconds> <message>"
+    return 1
+  fi
+  (sleep "$1" && echo "Reminder: $2") &
+}
+
+# rerun last command with sudo
+please() {
+  sudo $(fc -ln -1)
+}
+
 # counts the lines of code in your current codebase
 # needs pnpm
 loc() {
